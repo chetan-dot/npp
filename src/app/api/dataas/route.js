99f21cf74f750d6b5d0cbbf5fb363debe5498a8d
Vikash Kumar Singh <kumarrohit93334@gmail.com>
2025-03-10 21:42:08 +0530

    modify startDate and endDate api

--- src/app/api/dataas/route.js
+++ src/app/api/dataas/route.js
@@ -2,28 +2,6 @@ import connectMongo from "@/db/db";
 import { NextResponse } from "next/server";
 import garbage_history from "@/models/garbage_history";
 connectMongo();
-// export const GET = async () => {
-//   try {
-//     const response = await fetch(
-//       process.env.NEXT_PUBLIC_HTTP_SERVICE + "/user/wards"
-//     );
-
-//     if (!response.ok) {
-//       throw new Error("Failed to fetch data");
-//     }
-
-//     const hh = await response.json();
-//     const { success, ...filteredData } = hh;
-//     const paylod = {
-//       historyData: JSON.stringify(filteredData),
-//     };
-
-//     const data = await garbage_history.create(paylod);
-//     return NextResponse.json(data);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
 
 export const GET = async () => {
   try {
@@ -32,7 +10,146 @@ export const GET = async () => {
   } catch (error) {
     console.error(error);
     return NextResponse.json(
-      { error: "Failed to fetch data" },
+      { error: "Failed to fetcsh data" },
+      { status: 500 }
+    );
+  }
+};
+
+// export const POST = async (req) => {
+//   try {
+//     const { searchParams } = new URL(req.url);
+//     const startDate = searchParams.get("start_date");
+//     const endDate = searchParams.get("end_date");
+//     let query = {};
+
+//     if (startDate && endDate) {
+//       query.createdAt = {
+//         $gte: new Date(startDate),
+//         $lte: new Date(endDate),
+//       };
+//     } else if (startDate) {
+//       query.createdAt = {
+//         $gte: new Date(startDate),
+//       };
+//     } else if (endDate) {
+//       query.createdAt = {
+//         $lte: new Date(endDate),
+//       };
+//     }
+//     const histories = await garbage_history.find(query);
+//     return NextResponse.json(
+//       { histories: histories, success: true },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.log("error ", error);
+//   }
+// };
+
+// export const POST = async (req) => {
+//   try {
+//     const { searchParams } = new URL(req.url);
+//     const startDate = searchParams.get("start_date");
+//     const endDate = searchParams.get("end_date");
+//     let query = {};
+//     if (startDate && endDate) {
+//       query.createdAt = {
+//         $gte: new Date(startDate),
+//         $lte: new Date(endDate),
+//       };
+//     } else if (startDate) {
+//       query.createdAt = {
+//         $gte: new Date(startDate),
+//       };
+//     } else if (endDate) {
+//       query.createdAt = {
+//         $lte: new Date(endDate),
+//       };
+//     }
+//     const histories = await garbage_history.find(query);
+//     const aggregatedWardsData = {};
+
+//     let totalCount = 0;
+//     histories.forEach((history) => {
+//       const historyData = JSON.parse(history.historyData);
+//       historyData.totalWards.forEach((ward) => {
+//         const {
+//           label,
+//           numberOfUser,
+//           total_no_house_covered,
+//         } = ward;
+
+//         if (!aggregatedWardsData[label]) {
+//           aggregatedWardsData[label] = {
+//             label,
+//             totalNumberOfUsers: 0,
+//             totalNoHouseCovered: 0,
+//             count: 0,
+//           };
+//         }
+//         aggregatedWardsData[label].totalNumberOfUsers += numberOfUser;
+//         aggregatedWardsData[label].totalNoHouseCovered +=
+//           total_no_house_covered;
+//         aggregatedWardsData[label].count += 1;
+
+//         totalCount++;
+//       });
+//     });
+//     const wardsData = Object.keys(aggregatedWardsData).map((label) => {
+//       const ward = aggregatedWardsData[label];
+//       return {
+//         label: ward.label,
+//         totalNumberOfUsers: ward.totalNumberOfUsers,
+//         totalNoHouseCovered: ward.totalNoHouseCovered,
+//         avgNumberOfUsers: ward.totalNumberOfUsers / ward.count,
+//         avgTotalHouseCovered: ward.totalNoHouseCovered / ward.count,
+//       };
+//     });
+//     return NextResponse.json(
+//       {
+//         wardsData,
+//         success: true,
+//       },
+//       { status: 200 }
+//     );
+//   } catch (error) {
+//     console.log("error ", error);
+//     return NextResponse.json(
+//       { success: false, message: "An error occurred" },
+//       { status: 500 }
+//     );
+//   }
+// };
+
+export const PATCH = async (req) => {
+  try {
+    const { _id, historyData } = await req.json();
+    if (!_id) {
+      return NextResponse.json({ error: "id is required" }, { status: 400 });
+    }
+
+    const updatedDetails = await garbage_history.findByIdAndUpdate(
+      _id,
+      historyData,
+      { new: true }
+    );
+
+    if (!updatedDetails) {
+      return NextResponse.json(
+        { error: "No record found with that user_id" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      { data: updatedDetails, success: true },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error("Error during update:", error);
+    return NextResponse.json(
+      { error: "Internal server issue", success: false },
       { status: 500 }
     );
   }
@@ -41,11 +158,9 @@ export const GET = async () => {
 export const POST = async (req) => {
   try {
     const { searchParams } = new URL(req.url);
-
     const startDate = searchParams.get("start_date");
     const endDate = searchParams.get("end_date");
     let query = {};
-
     if (startDate && endDate) {
       query.createdAt = {
         $gte: new Date(startDate),
@@ -60,12 +175,42 @@ export const POST = async (req) => {
         $lte: new Date(endDate),
       };
     }
-    const histories = await garbage_history.find(query).select("-__v");
+    const histories = await garbage_history.find(query);
+    let totalUsers = 0;
+    let totalHousesCovered = 0;
+    let totalWardsCount = 0;
+
+    histories.forEach((history) => {
+      const historyData = JSON.parse(history.historyData);
+      historyData.totalWards.forEach((ward) => {
+        totalUsers += ward.numberOfUser;
+        totalHousesCovered += ward.total_no_house_covered;
+        totalWardsCount += 1;
+      });
+    });
+
+    const averageUsers = totalUsers / totalWardsCount;
+    const averageHousesCovered = totalHousesCovered / totalWardsCount;
+
     return NextResponse.json(
-      { histories: histories, success: true },
+      {
+        histories: [
+          {
+            totalUsers,
+            totalHousesCovered,
+            averageUsers,
+            averageHousesCovered,
+          },
+        ],
+        success: true,
+      },
       { status: 200 }
     );
   } catch (error) {
     console.log("error ", error);
+    return NextResponse.json(
+      { success: false, message: "An error occurred" },
+      { status: 500 }
+    );
   }
 };
